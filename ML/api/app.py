1fe31a21c77a11be380bd9d1edfae374ad8cd3b8
AnandKumar-099 <162425620+AnandKumar-099@users.noreply.github.com>
2026-04-08 21:42:20 +0530

    Update app to use environment variable for port

--- ML/api/app.py
+++ ML/api/app.py
@@ -221,4 +221,6 @@ def recommend():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
